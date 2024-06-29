023148e929e99c7e0fae806de2d529e876e44037
Alastear <alastearalize@gmail.com>
2024-06-29 16:54:58 +0700

    update text

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,8 +6,13 @@ const Product = () => {
 
     return (
         <div className='pt-20 justify-center text-center bg-black xl:h-[1000px] md:h-[800px] sm:h-[650px] h-[500px]' id="product" >
-            <div className='flex md:h-[75px] h-[50px] justify-center text-white text-center text-[36px] font-bold'>
-                PRODUCTS
+            <div className='md:h-[75px] h-[50px] justify-center text-center'>
+                <p className='text-[36px] font-bold text-white'>
+                    PRODUCTS
+                </p>
+                <p className="text-slate-500 lg:px-32 md:px-24 sm:px-14 px-0 antialiased">
+                    BLP Goods
+                </p>
             </div>
             <div className={`${'w-full md:pt-10 pt-10 flex justify-center'}`} >
                 <div className='flex w-auto xl:h-[650px] lg:h-[600px] md:h-[500px] sm:h-[400px] h-[300px] lg:px-[200px] mx-4'>
@@ -25,7 +30,7 @@ const Product = () => {
                 </div>
             </div>
             <div className='flex justify-center w-screen text-white'>
-            
+
             </div>
         </div>
     );

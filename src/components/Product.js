74ae39b9c3016a6259c870d74a345d63ae0b36aa
Alastear <42664787+Alastear@users.noreply.github.com>
2024-07-26 02:14:12 +0700

    update vp

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useIsVisible } from './useIsVisible';
 import Image from 'next/image';
 
 const Product = () => {
+    const [imageIndex, setImageIndex] = useState(0);
+    const imageList = [
+        "/images/product-01.jpeg",
+        "/images/Graphic_VP.png",
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImageIndex((prevIndex) => {
+                if (prevIndex === imageList.length - 1) {
+                    return 0;
+                } else {
+                    return prevIndex + 1;
+                }
+            });
+        }, 10000);
+        return () => clearInterval(interval);
+    }
+        , []);
+
 
     return (
         <div className='pt-20 justify-center text-center bg-black ' id="product" >
@@ -16,18 +36,43 @@ const Product = () => {
             </div>
             <div className={`${'w-full pb-10 pt-10 flex justify-center'}`} >
                 <div className='flex justify-center h-auto xl:mx-20 lg:mx-16 md:mx-12 sm:mx-8 mx-4'>
-                    <div className='xl:w-[1200px] lg:w-[1000px] w-full content-center justify-center text-center rounded-about text-white'>
-                        <Image
-                            src='/images/product-01.jpeg'
-                            alt='blp-main-image'
-                            width={0}
-                            height={0}
-                            loading="lazy"
-                            sizes="200vw"
-                            style={{ width: 'auto', height: 'auto' }}
-                        />
+                    <div className='flex xl:w-[1200px] lg:w-[1000px] w-full content-center justify-center text-center rounded-about text-white'>
+                        {
+                            imageList.map((image, index) => {
+                                return (
+                                    <Image
+                                        key={index}
+                                        src={image}
+                                        className={`
+                                        ${'transition ease-in duration-300 object-contain md:min-h-[600px] md:max-h-[600px] min-h-[275px] max-h-[275px]'}
+                                        ${index === imageIndex ? 'opacity-100' : 'opacity-0 hidden'}
+                                        `}
+                                        alt='blp-main-image'
+                                        width={0}
+                                        height={0}
+                                        loading="lazy"
+                                        sizes="100vw"
+                                        style={{ width: 'auto', height: 'auto' }}
+                                    />
+                                );
+                            })
+                        }
                     </div>
                 </div>
+            </div>
+            <div className='flex justify-center items-center text-white mb-4'>
+                {
+                    imageList.map((image, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`cursor-pointer w-4 h-4 mx-2 rounded-full ${index === imageIndex ? 'bg-white' : 'bg-slate-500'}`}
+                                onClick={() => setImageIndex(index)}
+                            >
+                            </div>
+                        );
+                    })
+                }
             </div>
             <div className='flex justify-center w-screen text-white'>
 
